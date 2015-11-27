96a22a600e4c9174cc8ee75080bff60be0e7060f
Daniel Blanco <danielblancob48@gmail.com>
2015-11-26 23:05:37 -0500

    new changes made to MOACTO part

--- main.m
+++ main.m
@@ -1,8 +1,42 @@
-clear all;
 clc;
-nodos = 0;
-while(nodos ~= 20 && nodos~=40 && nodos~=60 && nodos~=80 && nodos~=100)
-    nodos = input('Escoja la cantidad de nodos a trabajar (Opciones: 20, 40, 60, 80, 100) -> ');
+clear all;
+probs=3;
+iter=30;
+%%minD=zeros(1,iter);
+%%maxD=zeros(1,iter);
+meaD=zeros(1,iter);
+
+%%minT=zeros(1,iter);
+%%maxT=zeros(1,iter);
+meaT=zeros(1,iter);
+
+tick=zeros(1,iter);
+stdD=0;
+stdT=0;
+vecMeaD=zeros(probs,4);
+vecMeaT=zeros(probs,4);
+for i=1:probs
+    for j=1:iter
+        clc;
+        disp(['Iteration ',  num2str(j)]);
+        title(['Iteracion ',num2str(j),' archivo ',num2str(i)]);
+       
+        [pD, pT, tick(j) ] = MOACO(i*20);
+        
+       % minD(j)=min(pD);
+        %maxD(j)=max(pD);
+        meaD(j)=mean(pD);
+        
+        %minT(j)=min(pT);
+        %maxD(j)=max(pT);
+        meaT(j)=max(pT);
+     end
+    %%vecMeaD(i,:)=[i*20 min(minD) max(maxD(1,:)) mean(meaD) std(meaD) mean(tick)];
+    %%vecMeaT(i,:)=[i*20 min(minT) max(maxT(1,:)) mean(meaT) std(meaT) mean(tick)];    
+    vecMeaD(i,:)=[i*20 mean(meaD) std(meaD) mean(tick)];
+    vecMeaT(i,:)=[i*20 mean(meaT) std(meaT) mean(tick)];  
+    csvwrite('spea_D.csv' ,vecMeaD);
+    csvwrite('spea_T.csv' ,vecMeaT); 
 end
-%[paretoX, paretoY] = MOACO(nodos);
-[pD, pY] = MOACO(nodos);
+csvwrite('spea_D.csv' ,vecMeaD);
+csvwrite('spea_T.csv' ,vecMeaT);    
